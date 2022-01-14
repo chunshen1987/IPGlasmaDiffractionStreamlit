e2c9149527d7b29b87447a4ea1550efac4c6d2a3
Chun Shen <chunshen1987@gmail.com>
2022-01-14 15:56:04 -0500

    add unit to dsigma/dt

--- IPGlasmaDiffraction_app.py
+++ IPGlasmaDiffraction_app.py
@@ -101,7 +101,7 @@ def main(emu):
     plt.xlim([0, 2.5])
     plt.ylim([1, 5e2])
     plt.xlabel(r"$t$ (GeV$^2$)")
-    plt.ylabel(r"$d\sigma/dt$")
+    plt.ylabel(r"$d\sigma/dt$ (nb/GeV${-2}$)")
     plt.text(1.8, 80, r"$W = 75$ GeV")
 
     st.pyplot(fig)
